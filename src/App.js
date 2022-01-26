93324e67f32aefe95072f5b1b59f0cb5a38da364
Sadok Mourad Mhiri <sadokmhiri@gmail.com>
2022-01-27 00:35:50 +0100

    adding 2 exemples

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Exemple1 from './components/exemple1/exemple1';
+import Exemple2 from './components/exemple2/exemple2';
 
 function App() {
+
+  const [exemple, setExemple] = useState(1);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setExemple(1)}>
+        Exemple 1
+      </button>
+      <button onClick={() => setExemple(2)}>
+        Exemple 2
+      </button>
+
+      <div className=''>
+        <div>
+        {
+          exemple === 1 && (
+            <Exemple1/>
+          )
+        }
+        </div>
+        <div>
+        {
+          exemple === 2 && (
+            <Exemple2/>
+          )
+        }
+        </div>
+      </div>
     </div>
   );
 }
